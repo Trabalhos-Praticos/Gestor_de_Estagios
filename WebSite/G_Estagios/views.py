54aa00219c4a994b9df17e2acb0f1fea9f329ba7
Andre Carvalho <andre.filipe.carvalho@outlook.pt>
2023-11-17 21:38:53 +0000

    .

--- WebSite/G_Estagios/views.py
+++ WebSite/G_Estagios/views.py
@@ -19,6 +19,9 @@ from supabase import create_client, Client
 
 url = "https://rxpmviaksgbicuyrxqtr.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ4cG12aWFrc2diaWN1eXJ4cXRyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5OTg2OTkwMywiZXhwIjoyMDE1NDQ1OTAzfQ.vE4o4iq3Y9bMPEcP8WFLlEVy6oUyqg7JhDLoK9hAOYI"
+
+
+
 supabase_client = supabase.create_client(url, key)
 
 def starting(request):
@@ -42,10 +45,14 @@ def Register(request):
             }
         })
         return HttpResponseRedirect(reverse('dash'))
+    
+    
 
 def D_V(request):
     if request.method== 'GET':
         return render(request,"G_Estagios/coiso.html",)
+
+
 
 def login_view(request):
     if request.method== 'GET':
@@ -53,9 +60,12 @@ def login_view(request):
     if request.method== 'POST':
         email = request.POST.get('Email')
         password= request.POST.get('Password')
-        data = supabase.auth.sign_in_with_password({"email": email, "password": password})
+        data = supabase_client.auth.sign_in_with_password({"email": email, "password": password})
         if data:
             return HttpResponseRedirect(reverse('dash'))
+      
+        
+        
         
 def logout_view(request):
     
@@ -67,14 +77,14 @@ def logout_view(request):
 
         # Verifica se o logout foi bem-sucedido
         if response is not None and response.get('error') is not None:
-            print(f"Erro ao fazer logout: {response['error']}")
+            print("Erro ao fazer logout: {response['error']}")
         else:
             response = supabase_client.auth.sign_out()
             print("Logout bem-sucedido!")
             # Faz logout usando Supabase
             # Verifica se o logout foi bem-sucedido
             if response.get('error') is not None:
-                print(f"Erro ao fazer logout: {response['error']}")
+                print("Erro ao fazer logout: {response['error']}")
             else:
                 # Redireciona para a página inicial ou outra página após o logout
                 return HttpResponseRedirect(reverse('login'))
