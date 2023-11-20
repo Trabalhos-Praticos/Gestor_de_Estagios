324498d54e83e95ecf18d65711fcca4297946ffb
Andre Carvalho <andre.filipe.carvalho@outlook.pt>
2023-11-20 23:35:57 +0000

    .

--- WebSite/G_Estagios/views.py
+++ WebSite/G_Estagios/views.py
@@ -1,13 +1,10 @@
-from django.http import HttpResponse
 from django.shortcuts import redirect, render, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.urls import reverse
-from .models import verify_email,CustomUser,Curso, verificar_palavra_passe,FormRegisto
+from .models import verify_email,CustomUser,Curso, verificar_palavra_passe
 from django.contrib.auth import authenticate, login, logout
-from django.core.exceptions import ObjectDoesNotExist
-from django.http import JsonResponse
-from django.contrib.auth.models import Group
+
 
 
 
@@ -20,6 +17,8 @@ def D_v(request):
         cursos=curso.objects.all()
         if request.user.is_authenticated:#and request.user.groups.filter(name='Admin').exists():
             return render(request, 'G_Estagios/dashboard.html',({'cursos': cursos, 'customusers': Users}))
+
+
 
 
 @login_required
@@ -65,27 +64,38 @@ def registo(request):
             messages.success(request,"Registo bem sucedido")
             return redirect('register')
 
-def registo_form(request):
-    if request.method == 'POST':
-        form = FormRegisto(request.POST)
-        if form.is_valid():
-            nome = form.cleaned_data['nome']
-            email = form.cleaned_data['email']
-            senha = form.cleaned_data['password']
-            # Verificar a segurança da palavra-passe
-            is_password_secure, error_message = verificar_palavra_passe(senha)
-            if is_password_secure:
-                user = CustomUser(username=email,first_name = nome)
-                user.set_password(senha)
-                user.save()
-                return redirect('pagina_sucesso')
-            else:
-                form.add_error('password', error_message)  # Adicione a mensagem de erro ao formulário
+def registo(request):
+    User=CustomUser
+    Email= request.POST.get('Email')
+    try:
+        V_Email = User.objects.get(username=Email)
+    except User.DoesNotExist:
+        V_Email = False
+    if V_Email:
+        messages.error(request,"Já existe um utilizador com esse email")
+        return redirect('register')
     else:
-        form = FormRegisto()
-    return render(request, 'G_Estagios/Register.html', {'form': form})
-    
-    
+        Password=request.POST.get('Password')
+        Nome = request.POST.get('Nome')
+        is_password_secure, error_message = verificar_palavra_passe(Password)
+        V_e = verify_email(Email)
+        if is_password_secure:
+            if V_e == "Invalido":
+                messages.error(request, 'Email invalido')
+                return redirect('register')
+            elif V_e == 'Aluno':
+                newuser = User.objects.create_user(username=Email,first_name=Nome,password=is_password_secure)
+                newuser.save()
+                messages.info(request,"Registo bem sucedido")
+                return redirect('register')
+            elif V_e == 'Professor':
+                newuser = User.objects.create_user(username=Email,first_name=Nome,password=is_password_secure)
+                newuser.save()
+                messages.success(request,"Registo bem sucedido")
+                return redirect('register')
+        else:
+            messages.error(request,error_message)
+            return redirect('register')
     
 def view_login(request):
     if request.method == "GET":
