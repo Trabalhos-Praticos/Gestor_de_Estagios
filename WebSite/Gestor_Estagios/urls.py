d88b54addfee3cec635a00e7ab37ec6711754318
Andre Carvalho <114291608+esquilo2233@users.noreply.github.com>
2023-12-12 17:49:02 +0000

    ṕoogk

--- WebSite/Gestor_Estagios/urls.py
+++ WebSite/Gestor_Estagios/urls.py
@@ -42,4 +42,6 @@ urlpatterns = [
     path('administracao/escola_curso/eliminar_curso/<int:curso_id>/', views.eliminar_curso, name='eliminar_curso'),
     path('administracao/escola_curso/editar_curso/<int:curso_id>/', views.editar_curso, name='editar_curso'),
     path('administracao/escola_curso/eliminar_polo/<int:polo_id>/', views.eliminar_polo, name='eliminar_polo'),
+    path('administracao/documentos',views.adm_docs,name='documentos_adm'),
+    path('upload_assiduidade/',views.add_Assiduidade,name = 'Upload_Assiduidade_adm'),
 ]
