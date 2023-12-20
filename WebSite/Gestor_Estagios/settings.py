643160306ac9acd43559371dc6bfe50dad5d058e
Andre Carvalho <andre.filipe.carvalho@outlook.pt>
2023-12-20 22:08:10 +0000

    .

--- WebSite/Gestor_Estagios/settings.py
+++ WebSite/Gestor_Estagios/settings.py
@@ -143,3 +143,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'G_Estagios.CustomUser'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
