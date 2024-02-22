15d1d0632ba0ed28bc02d063c68c1e089ab6a680
Lucas Cruz <124691704+ludvikcruz@users.noreply.github.com>
2024-02-22 09:43:32 +0000

    vdz

--- WebSite/cypress/e2e/Test/apitest.cy.js
+++ WebSite/cypress/e2e/Test/apitest.cy.js
@@ -1,16 +1,6 @@
 describe('api test', () => {
     it('GET - all users', () => {
            
-        cy.request({
-            method: 'GET',
-            url: '/api/users/',
-            headers: {
-              'Accept': 'application/json'
-            }
-          }).then((response) => {
-            expect(response.status).to.eq(200);
-            expect(response.body).to.be.an('array');
-          });
-          
+    cy.api('/api/users');
     });
 })
